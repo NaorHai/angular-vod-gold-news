b2299f6b2c09c6260e06a8ff52b2c1761d994ec0
NaorHai <Na1994or>
2017-06-06 22:04:47 +0300

    added news,reporters,top-rated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NewsComponent } from './news/news.component';
+import { ReportetsComponent } from './reportets/reportets.component';
+import { HomeComponent } from './home/home.component';
+import { TopRatedComponent } from './top-rated/top-rated.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewsComponent
+    NewsComponent,
+    ReportetsComponent,
+    HomeComponent,
+    TopRatedComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'news',
+        component: NewsComponent
+      },
+      {
+        path: 'reporters',
+        component: ReportetsComponent
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'top',
+        component: TopRatedComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reportets/reportets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reportets',
+  templateUrl: './reportets.component.html',
+  styleUrls: ['./reportets.component.css']
+})
+export class ReportetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
