aefcf70df849643a4930bd1f1c193e54ab8b8e7b
NaorHai <Na1994or>
2017-06-09 15:03:46 +0300

    added sorting to the top rated news fixed styling

--- src/app/top-rated/top-rated.component.ts
+++ src/app/top-rated/top-rated.component.ts
@@ -8,6 +8,7 @@ import  { News } from '../news/news';
   templateUrl: './top-rated.component.html',
   styleUrls: ['./top-rated.component.css']
 })
+
 export class TopRatedComponent implements OnInit {
   ratedNews : News[];
   top: number = 8; //top rated
@@ -18,7 +19,19 @@ export class TopRatedComponent implements OnInit {
     this.apiService.getVodRatedHigherThan(this.top)
       .then((news : News[]) => {
         this.ratedNews = news;
+        this.sort(this.ratedNews);
       });
   }
 
+  private sort (unsortedNews: News[]) {
+    return unsortedNews.sort((un1,un2) => {
+      if (un1.rate < un2.rate) {
+        return 1;
+      }
+      if (un1.rate > un2.rate) {
+        return -1;
+      }
+      return 0;
+    });
+  }
 }
